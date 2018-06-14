894ce645f373a8ffa394901c15f9d961897f42f0
nana.shimizu <nana.shimizu@rakuten.com>
2018-06-14 07:47:22 +0000

    timeline

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ Route::get('/', 'MicropostsController@index');
 
 // user registration
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-// Login authentication
+//  Login authentication
+ 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
@@ -33,7 +35,6 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('followings', 'UsersController@followings')->name('users.followings');
         Route::get('followers', 'UsersController@followers')->name('users.followers');
     });
-    
     
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
